beef4aa883dddfa71b4b80087a19132728a6c651
Dimitar Simeonov <simeonov.dimitar@gmail.com>
2012-02-20 11:31:32 -0500

    saving

---
+++ aliases.sh
@@ -0,0 +1,13 @@
+export TERM=xterm-256color
+
+# shorthand for emacs
+alias em='emacs -nw'
+
+# for easy editing running this file
+alias al='em ~/aliases.sh'
+alias xal='source ~/aliases.sh'
+
+# aliases
+alias gpx='cd ~/github_projects/gpxstats'
+alias sls='screen -ls'
+alias lchrome='google-chrome --allow-file-access-from-files'
